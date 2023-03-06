422d38d51e1719e3e8f6cfbd3802daf4a4ce4004
di-sukharev <dim.sukharev@gmail.com>
2023-03-06 21:41:58 +0800

    * 🐛 fix(githook.ts): update HOOK_PATH to point to the correct file path The HOOK_PATH variable was pointing to the wrong file path, which caused the hook to fail. The file path has been updated to point to the correct file path.

--- src/commands/githook.ts
+++ src/commands/githook.ts
@@ -20,7 +20,7 @@ export const hookCommand = command(
     parameters: ['<set/unset>']
   },
   async (argv) => {
-    const HOOK_PATH = fileURLToPath(new URL('cli.cjs', import.meta.url));
+    const HOOK_PATH = fileURLToPath(new URL('out/cli.cjs', import.meta.url));
 
     try {
       await assertGitRepo();
