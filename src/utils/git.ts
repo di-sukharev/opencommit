fc6bcbf393b240471e8258fd2b10c849bb40d19c
Vladyslav Kapkan <vladickbaraza@gmail.com>
2023-03-14 20:03:52 +0100

    * chore(git.ts): import text function from @clack/prompts package
    * refactor(git.ts): remove excludeBigFilesFromDiff constant and filter out .lock files from getStagedFiles and getChangedFiles functions
    * feat(git.ts): add error message when all staged files are .lock files
    * feat(git.ts): add error message when all changed files are .lock files
    * feat(git.ts): add warning message when some files are .lock files and excluded from git add and git diff

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,5 +1,5 @@
 import { execa } from 'execa';
-import { spinner } from '@clack/prompts';
+import { spinner, text } from '@clack/prompts';
 
 export const assertGitRepo = async () => {
   try {
@@ -9,45 +9,90 @@ export const assertGitRepo = async () => {
   }
 };
 
-const excludeBigFilesFromDiff = ['*-lock.*', '*.lock'].map(
-  (file) => `:(exclude)${file}`
-);
-
 export const getStagedFiles = async (): Promise<string[]> => {
   const { stdout: files } = await execa('git', [
     'diff',
     '--name-only',
-    '--cached',
-    ...excludeBigFilesFromDiff
+    '--cached'
   ]);
 
   if (!files) return [];
+
+  const excludedFiles = files
+    .split('\n')
+    .filter((file) => file.includes('.lock') || file.includes('-lock.'));
+  if (excludedFiles.length === files.split('\n').length) {
+    throw new Error(
+      `Staged files are .lock files which are excluded by default as it's too big, commit it yourself, don't waste your api tokens. \n${excludedFiles.join(
+        '\n'
+      )}`
+    );
+  }
 
   return files.split('\n').sort();
 };
 
 export const getChangedFiles = async (): Promise<string[]> => {
   const { stdout: modified } = await execa('git', ['ls-files', '--modified']);
-  const { stdout: others } = await execa('git', ['ls-files', '--others', '--exclude-standard']);
+  const { stdout: others } = await execa('git', [
+    'ls-files',
+    '--others',
+    '--exclude-standard'
+  ]);
 
   const files = [...modified.split('\n'), ...others.split('\n')];
 
-  return files.filter(Boolean).sort();
+  const filesWithoutLocks = files.filter(
+    (file) => !file.includes('.lock') && !file.includes('-lock.')
+  );
+
+  if (filesWithoutLocks.length === 0) {
+    throw new Error(
+      `No files to commit. All files are .lock files which are excluded by default as it's too big, commit it yourself, don't waste your api tokens. \n${files
+        .filter((file) => file.includes('.lock') || file.includes('-lock.'))
+        .join('\n')}`
+    );
+  }
+
+  return filesWithoutLocks.sort();
 };
 
 export const gitAdd = async ({ files }: { files: string[] }) => {
+  const filteredFiles = files.filter(
+    (file) => !file.includes('.lock') && !file.includes('-lock.')
+  );
+
   const gitAddSpinner = spinner();
   gitAddSpinner.start('Adding files to commit');
-  await execa('git', ['add', ...files]);
+  await execa('git', ['add', ...filteredFiles]);
   gitAddSpinner.stop('Done');
+
+  if (filteredFiles.length !== files.length) {
+    text({
+      message: `Some files are .lock files which are excluded by default as it's too big, commit it yourself, don't waste your api tokens. \n${files
+        .filter((file) => file.includes('.lock') || file.includes('-lock.'))
+        .join('\n')}`
+    });
+  }
 };
 
 export const getDiff = async ({ files }: { files: string[] }) => {
+  const filesWithoutLocks = files.filter(
+    (file) => !file.includes('.lock') && !file.includes('-lock.')
+  );
+
+  if (filesWithoutLocks.length !== files.length) {
+    text({
+      message: `Some files are .lock files which are excluded by default as it's too big, commit it yourself, don't waste your api tokens. \n${files
+        .filter((file) => file.includes('.lock') || file.includes('-lock.'))
+        .join('\n')}`
+    });
+  }
+
   const { stdout: diff } = await execa('git', [
     'diff',
     '--staged',
-    ...files,
-    ...excludeBigFilesFromDiff
+    ...filesWithoutLocks
   ]);
 
   return diff;
