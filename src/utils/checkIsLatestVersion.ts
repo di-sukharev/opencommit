83c77eb5009389b4e4c9ed1e8c75effa0e3b2307
nader.zouaoui <nader.zouaoui@edonec.com>
2023-03-17 16:46:33 +0100

    ✨ feat(cli.ts): add checkIsLatestVersion function
    This commit adds a new function that checks if the current version of OpenCommit is the latest version. The function uses the getOpenCommitLatestVersion function from the api module to get the latest version of OpenCommit. If the current version is not the latest version, a warning message is printed to the console, informing the user to update to the latest version to get the latest features and bug fixes.

---
+++ src/utils/checkIsLatestVersion.ts
@@ -0,0 +1,23 @@
+import { getOpenCommitLatestVersion } from '../api';
+import currentPackage from '../../package.json' assert { type: 'json' };
+import chalk from 'chalk';
+export const checkIsLatestVersion = async () => {
+  const latestVersion = await getOpenCommitLatestVersion();
+
+  if (latestVersion) {
+    const currentVersion = currentPackage.version;
+
+    if (currentVersion !== latestVersion) {
+      console.warn(
+        chalk.yellow(
+          `
+You are not using the latest stable version of OpenCommit!
+consider updating to the latest version to get the latest features and bug fixes.
+Current version: ${currentVersion}
+Latest version: ${latestVersion}
+        `
+        )
+      );
+    }
+  }
+};
