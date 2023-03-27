3302d44845eaea8b827657ad6d636e4c1a596a39
Dang Quang Linh <dangquanglinh48@gmail.com>
2023-03-27 14:54:40 +0700

    feat(i18n): add support for Vietnamese language and translation for commit messages in Vietnamese

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -7,6 +7,7 @@ import zh_CN from '../i18n/zh_CN.json' assert { type: 'json' };
 import zh_TW from '../i18n/zh_TW.json' assert { type: 'json' };
 import ja from '../i18n/ja.json' assert { type: 'json' };
 import pt_br from '../i18n/pt_br.json' assert { type: 'json' };
+import vi_VN from '../i18n/vi_VN.json' assert { type: 'json' };
 
 export enum I18nLocals {
   'en' = 'en',
@@ -29,7 +30,8 @@ export const i18n = {
   fr,
   it,
   ko,
-  pt_br
+  pt_br,
+  vi_VN
 };
 
 export const I18N_CONFIG_ALIAS: { [key: string]: string[] } = {
@@ -41,6 +43,7 @@ export const I18N_CONFIG_ALIAS: { [key: string]: string[] } = {
   fr: ['fr', 'French', 'française'],
   it: ['it', 'Italian', 'italiano'],
   pt_br: ['pt_br', 'Portuguese', 'português'],
+  vi_VN: ['vi_VN', 'Vietnamese', 'tiếng Việt'],
   en: ['en', 'English', 'english'],
 };
 
