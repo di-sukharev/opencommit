3edb6e2fc11dd712bce05353e9585e23b1c0f0f2
di-sukharev <dim.sukharev@gmail.com>
2023-03-29 18:23:25 +0800

    chore(generateCommitMessageFromGitDiff.ts): fix typo in GitMoji convention

--- src/generateCommitMessageFromGitDiff.ts
+++ src/generateCommitMessageFromGitDiff.ts
@@ -16,8 +16,8 @@ const INIT_MESSAGES_PROMPT: Array<ChatCompletionRequestMessage> = [
     role: ChatCompletionRequestMessageRoleEnum.System,
     // prettier-ignore
     content: `You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the conventional commit convention and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message.
-${config?.emoji? 'Use Gitmoji convention to preface the commit.': 'Do not preface the commit with anything.'}
-${config?.description  ? 'Add a short description of why the commit is done after the commit message. Don\'t start it with "This commit", just describe the changes.': "Don't add any descriptions to the commit, only commit message."}
+${config?.emoji? 'Use GitMoji convention to preface the commit.': 'Do not preface the commit with anything.'}
+${config?.description  ? 'Add a short description of WHY the changes are done after the commit message. Don\'t start it with "This commit", just describe the changes.': "Don't add any descriptions to the commit, only commit message."}
 Use the present tense. Lines must not be longer than 74 characters. Use ${translation.localLanguage} to answer.`
   },
   {
