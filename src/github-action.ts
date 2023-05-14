707d90de1c13b241d7763ce1c66231c2e180f5ff
di-sukharev <dim.sukharev@gmail.com>
2023-05-14 23:34:51 +0800

    feat(package.json): add @octokit/webhooks-schemas and @octokit/webhooks-types for better webhook handling refactor(github-action.ts): replace child_process with execa for better command execution refactor(github-action.ts): use octokit/webhooks-types for improved typing and handling of GitHub events feat(github-action.ts): add support for improving commit messages on push and pull_request events

--- src/github-action.ts
+++ src/github-action.ts
@@ -1,67 +1,78 @@
-import * as child_process from 'child_process';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import core from '@actions/core';
+import github from '@actions/github';
+import { execa } from 'execa';
+import { Commit, PushEvent } from '@octokit/webhooks-types';
+import { intro, outro, spinner } from '@clack/prompts';
+import { PullRequestEvent } from '@octokit/webhooks-types';
 
-// TODO: add all OpenCommit configs
+// This should be a token with access to your repository scoped in as a secret.
+// The YML workflow will need to set GITHUB_TOKEN with the GitHub Secret Token
+// GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+// https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
+const context = github.context;
+const owner = context.repo.owner;
+const repo = context.repo.repo;
 
-// TODO: replace with execa package
-function execShellCommand(cmd: string) {
-  return new Promise<string>((resolve, reject) => {
-    child_process.exec(
-      cmd,
-      (
-        error: child_process.ExecException | null,
-        stdout: string,
-        stderr: string
-      ) => {
-        if (error) {
-          reject(stderr);
-        } else {
-          resolve(stdout);
-        }
-      }
+async function improveCommitMessagesWithRebase(commits: Commit[]) {
+  const commitsToImprove = [];
+  // TODO: fix the error
+  for (const commit of commits) {
+    if (commit.message === 'oc' && commit.distinct) {
+      commitsToImprove.push(commit);
+    }
+  }
+
+  if (commitsToImprove.length) {
+    const commitSpinner = spinner();
+    commitSpinner.start(
+      `found ${commitsToImprove.length} commits with a message "oc", improving`
     );
-  });
+
+    // Start an interactive rebase
+    await execa('git', ['rebase', '-i', commitsToImprove.join(' ').trim()]);
+
+    for (const commit of commitsToImprove) {
+      // Question: how to get commit diff here?
+      const improvedMessage = `improved: ${commit.id}`;
+
+      await execa('git', ['commit', '--amend', '-m', improvedMessage]);
+      await execa('git', ['rebase', '--continue']);
+    }
+
+    // Force push the rebased commits
+    await execa('git', ['push', 'origin', `+${context.ref}`]);
+
+    commitSpinner.stop('📝 Commit messages improved with a `rebase -i`');
+  } else {
+    console.log(
+      'No commits with a message "oc" found. If you want OpenCommit to generate a commit message for you — leave the message as "oc".'
+    );
+  }
 }
 
 async function run() {
+  intro('OpenCommit — improving commit messages with GPT');
   try {
-    const token = core.getInput('token');
-    const octokit = github.getOctokit(token);
+    if (github.context.eventName === 'push') {
+      const payload = github.context.payload as PushEvent;
 
-    const context = github.context;
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
+      return await improveCommitMessagesWithRebase(payload.commits);
+    } else if (
+      github.context.eventName === 'pull_request' &&
+      github.context.payload.action === 'opened'
+    ) {
+      const payload = github.context.payload as PullRequestEvent;
+      // Question: how to get proper Input type
+      const commits = payload.pull_request.commits_url;
 
-    const commits = await octokit.rest.repos.get({
-      owner: owner,
-      repo: repo
-    });
-
-    let shasToReword = '';
-
-    // TODO: fix the error
-    for (const commit of commits) {
-      if (commit.commit.message.includes('oc')) {
-        shasToReword += `${commit.sha} `;
-      }
-    }
-
-    if (shasToReword) {
-      // Start an interactive rebase
-      await execShellCommand(`git rebase -i ${shasToReword.trim()}`);
-
-      // For each commit to be modified, change the message
-      for (const sha of shasToReword.split(' ')) {
-        // TODO: call openAI
-        const improvedMessage = 'TODO';
-        await execShellCommand(
-          `git commit --amend -m "$${improvedMessage}" && git rebase --continue`
-        );
-      }
-
-      // Force push the rebased commits
-      await execShellCommand(`git push origin +${context.ref}`);
+      return await improveCommitMessagesWithRebase(commits);
+    } else {
+      outro('wrong action');
+      core.error(
+        `OpenCommit was called on ${github.context.payload.action}. OpenCommit is not supposed to be used on actions other from "pull_request.opened" and "push".`
+      );
     }
   } catch (error: any) {
     // TODO: fix any after test
