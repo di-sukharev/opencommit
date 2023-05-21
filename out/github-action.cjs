f506c1287ca1cdbdb77d79854e2380a2f3b3160b
di-sukharev <dim.sukharev@gmail.com>
2023-05-21 12:05:42 +0800

    build

--- out/github-action.cjs
+++ out/github-action.cjs
@@ -27291,27 +27291,20 @@ async function improveCommitMessagesWithRebase({
   (0, import_fs2.writeFileSync)(
     `./rebase-exec.sh`,
     `
-#!/bin/bash
-count=$(cat count.txt)
-git commit --amend -F commit-$count.txt
-echo $(( count + 1 )) > count.txt
-        `
+  #!/bin/bash
+  count=$(cat count.txt)
+  git commit --amend -F commit-$count.txt
+  echo $(( count + 1 )) > count.txt
+          `
   );
   await import_exec.default.exec(`chmod +x ./rebase-exec.sh`);
-  await import_exec.default.exec(
-    `git`,
-    ["rebase", `${commitsToImprove[0].sha}^`, "--exec", "./rebase-exec.sh"],
+  await execPromise(
+    `git rebase ${commitsToImprove[0].sha}^ --exec "git commit --amend -F ./rebase-exec.sh"`,
     {
       env: {
         GIT_SEQUENCE_EDITOR: 'sed -i -e "s/^pick/reword/g"',
         GIT_COMMITTER_NAME: process.env.GITHUB_ACTOR,
         GIT_COMMITTER_EMAIL: `${process.env.GITHUB_ACTOR}@users.noreply.github.com`
-      },
-      listeners: {
-        stderr: async (data) => {
-          ce(`Error: ${data.toString()}`);
-          await import_exec.default.exec("git", ["rebase", "abort"]);
-        }
       }
     }
   );
--- src/github-action.ts
+++ src/github-action.ts
@@ -185,29 +185,22 @@ async function improveCommitMessagesWithRebase({
   writeFileSync(
     `./rebase-exec.sh`,
     `
-#!/bin/bash
-count=$(cat count.txt)
-git commit --amend -F commit-$count.txt
-echo $(( count + 1 )) > count.txt
-        `
+  #!/bin/bash
+  count=$(cat count.txt)
+  git commit --amend -F commit-$count.txt
+  echo $(( count + 1 )) > count.txt
+          `
   );
 
   await exec.exec(`chmod +x ./rebase-exec.sh`);
 
-  await exec.exec(
-    `git`,
-    ['rebase', `${commitsToImprove[0].sha}^`, '--exec', './rebase-exec.sh'],
+  await execPromise(
+    `git rebase ${commitsToImprove[0].sha}^ --exec "git commit --amend -F ./rebase-exec.sh"`,
     {
       env: {
         GIT_SEQUENCE_EDITOR: 'sed -i -e "s/^pick/reword/g"',
         GIT_COMMITTER_NAME: process.env.GITHUB_ACTOR!,
         GIT_COMMITTER_EMAIL: `${process.env.GITHUB_ACTOR}@users.noreply.github.com`
-      },
-      listeners: {
-        stderr: async (data) => {
-          outro(`Error: ${data.toString()}`);
-          await exec.exec('git', ['rebase', 'abort']);
-        }
       }
     }
   );
