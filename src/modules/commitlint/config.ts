d0d571d1206e296af905106d5131ab4d0ffed3c8
Sébastien Fichot <sebastien.fichot@telaqua.com>
2023-06-18 20:16:54 +0200

    feat(commitlint): add function to configure commitlint integration This commit adds a new function called `configureCommitlintIntegration` which is responsible for configuring the commitlint integration. The function reads the commitlint configuration and generates consistency with the given commitlint rules. If the configuration file already exists, the function checks if the hash of the existing configuration is the same as the hash of the new configuration. If the hashes are the same, the function does not update the configuration file unless the `force` flag is set to true. The function then generates consistency with the given commitlint rules and writes the new configuration to the file system.

---
+++ src/modules/commitlint/config.ts
@@ -0,0 +1,80 @@
+import { spinner } from '@clack/prompts';
+
+import { api } from '../../api';
+import { i18n, I18nLocals } from '../../i18n';
+import { getConfig } from '../../commands/config';
+
+import { CommitlintLLMConfig } from './types';
+import { COMMITLINT_LLM_CONFIG_PATH } from './constants';
+import { getCommitLintPWDConfig } from './pwd-commitlint';
+import * as utils from './utils';
+import { computeHash } from './crypto';
+import { inferPromptsFromCommitlintConfig, commitlintPrompts } from './prompts';
+
+const config = getConfig();
+const translation = i18n[(config?.OCO_LANGUAGE as I18nLocals) || 'en'];
+
+export const configureCommitlintIntegration = async (force = false) => {
+  const spin = spinner();
+  spin.start('Loading @commitlint configuration');
+
+  const fileExists = await utils.commitlintLLMConfigExists();
+
+  let commitLintConfig = await getCommitLintPWDConfig();
+
+  // debug complete @commitlint configuration
+  // await fs.writeFile(
+  //   `${OPENCOMMIT_COMMITLINT_CONFIG}-commitlint-debug`,
+  //   JSON.stringify(commitLintConfig, null, 2)
+  // );
+
+  const hash = await computeHash(JSON.stringify(commitLintConfig));
+
+  spin.stop(`Read @commitlint configuration (hash: ${hash})`);
+
+  if (fileExists) {
+    // Check if we need to update the prompts.
+    const { hash: existingHash } = await utils.getCommitlintLLMConfig();
+    if (hash === existingHash && !force) {
+      spin.stop(
+        'Hashes are the same, no need to update the config. Run "force" command to bypass.'
+      );
+      return;
+    }
+  }
+
+  spin.start('Generating consistency with given @commitlint rules');
+
+  const prompts = inferPromptsFromCommitlintConfig(commitLintConfig);
+
+  const consistencyPrompts =
+    commitlintPrompts.GEN_COMMITLINT_CONSISTENCY_PROMPT(prompts);
+
+  // debug prompt which will generate a consistency
+  // await fs.writeFile(
+  //   `${COMMITLINT_LLM_CONFIG}-debug`,
+  //   consistencyPrompts.map((p) => p.content)
+  // );
+
+  let consistency =
+    (await api.generateCommitMessage(consistencyPrompts)) || '{}';
+
+  // Cleanup the consistency answer. Sometimes 'gpt-3.5-turbo' sends rule's back.
+  prompts.forEach((prompt) => (consistency = consistency.replace(prompt, '')));
+  // ... remaining might be extra set of "\n"
+  consistency = utils.removeDoubleNewlines(consistency);
+
+  const commitlintLLMConfig: CommitlintLLMConfig = {
+    hash,
+    prompts,
+    consistency: {
+      [translation.localLanguage]: {
+        ...JSON.parse(consistency as string)
+      }
+    }
+  };
+
+  await utils.writeCommitlintLLMConfig(commitlintLLMConfig);
+
+  spin.stop(`Done - please review contents of ${COMMITLINT_LLM_CONFIG_PATH}`);
+};
