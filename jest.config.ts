1b25aabbb83fbec8b525a08b9e201a9d7275481d
di-sukharev <dim.sukharev@gmail.com>
2023-09-03 17:12:22 +0800

    configure jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  // [...]
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  }
+};
+
+export default jestConfig;
