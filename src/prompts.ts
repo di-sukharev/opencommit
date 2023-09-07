d9fbc0ee3fe53f2af799d775fca41cf8b47a02ec
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-09-07 09:45:32 +0200

    📝 docs(prompts.ts): update prompt message to include information about GitMoji convention and descriptions of changes
    📝 docs(prompts.ts): update prompt message to include information about GitMoji convention and descriptions of changes

--- src/prompts.ts
+++ src/prompts.ts
@@ -21,11 +21,84 @@ export const IDENTITY =
 const INIT_MAIN_PROMPT = (language: string): ChatCompletionRequestMessage => ({
   role: ChatCompletionRequestMessageRoleEnum.System,
   content: `${IDENTITY} Your mission is to create clean and comprehensive commit messages as per the conventional commit convention and explain WHAT were the changes and mainly WHY the changes were done. I'll send you an output of 'git diff --staged' command, and you are to convert it into a commit message.
-    ${
-      config?.OCO_EMOJI
-        ? 'Use GitMoji convention to preface the commit.'
-        : 'Do not preface the commit with anything.'
-    }
+  ${
+    config?.OCO_EMOJI
+      ? 'Use GitMoji convention to preface the commit. Here are some help to choose the right emoji (emoji, description): ' +
+        '🎨, Improve structure / format of the code; ' +
+        '⚡️, Improve performance; ' +
+        '🔥, Remove code or files; ' +
+        '🐛, Fix a bug; ' +
+        '🚑️, Critical hotfix; ' +
+        '✨, Introduce new features; ' +
+        '📝, Add or update documentation; ' +
+        '🚀, Deploy stuff; ' +
+        '💄, Add or update the UI and style files; ' +
+        '🎉, Begin a project; ' +
+        '✅, Add, update, or pass tests; ' +
+        '🔒️, Fix security issues; ' +
+        '🔐, Add or update secrets; ' +
+        '🔖, Release / Version tags; ' +
+        '🚨, Fix compiler / linter warnings; ' +
+        '🚧, Work in progress; ' +
+        '💚, Fix CI Build; ' +
+        '⬇️, Downgrade dependencies; ' +
+        '⬆️, Upgrade dependencies; ' +
+        '📌, Pin dependencies to specific versions; ' +
+        '👷, Add or update CI build system; ' +
+        '📈, Add or update analytics or track code; ' +
+        '♻️, Refactor code; ' +
+        '➕, Add a dependency; ' +
+        '➖, Remove a dependency; ' +
+        '🔧, Add or update configuration files; ' +
+        '🔨, Add or update development scripts; ' +
+        '🌐, Internationalization and localization; ' +
+        '✏️, Fix typos; ' +
+        '💩, Write bad code that needs to be improved; ' +
+        '⏪️, Revert changes; ' +
+        '🔀, Merge branches; ' +
+        '📦️, Add or update compiled files or packages; ' +
+        '👽️, Update code due to external API changes; ' +
+        '🚚, Move or rename resources (e.g.: files, paths, routes); ' +
+        '📄, Add or update license; ' +
+        '💥, Introduce breaking changes; ' +
+        '🍱, Add or update assets; ' +
+        '♿️, Improve accessibility; ' +
+        '💡, Add or update comments in source code; ' +
+        '🍻, Write code drunkenly; ' +
+        '💬, Add or update text and literals; ' +
+        '🗃️, Perform database related changes; ' +
+        '🔊, Add or update logs; ' +
+        '🔇, Remove logs; ' +
+        '👥, Add or update contributor(s); ' +
+        '🚸, Improve user experience / usability; ' +
+        '🏗️, Make architectural changes; ' +
+        '📱, Work on responsive design; ' +
+        '🤡, Mock things; ' +
+        '🥚, Add or update an easter egg; ' +
+        '🙈, Add or update a .gitignore file; ' +
+        '📸, Add or update snapshots; ' +
+        '⚗️, Perform experiments; ' +
+        '🔍️, Improve SEO; ' +
+        '🏷️, Add or update types; ' +
+        '🌱, Add or update seed files; ' +
+        '🚩, Add, update, or remove feature flags; ' +
+        '🥅, Catch errors; ' +
+        '💫, Add or update animations and transitions; ' +
+        '🗑️, Deprecate code that needs to be cleaned up; ' +
+        '🛂, Work on code related to authorization, roles and permissions; ' +
+        '🩹, Simple fix for a non-critical issue; ' +
+        '🧐, Data exploration/inspection; ' +
+        '⚰️, Remove dead code; ' +
+        '🧪, Add a failing test; ' +
+        '👔, Add or update business logic; ' +
+        '🩺, Add or update healthcheck; ' +
+        '🧱, Infrastructure related changes; ' +
+        '🧑‍💻, Improve developer experience; ' +
+        '💸, Add sponsorships or money related infrastructure; ' +
+        '🧵, Add or update code related to multithreading or concurrency; ' +
+        '🦺, Add or update code related to validation.'
+      : 'Do not preface the commit with anything.'
+  }  
     ${
       config?.OCO_DESCRIPTION
         ? 'Add a short description of WHY the changes are done after the commit message. Don\'t start it with "This commit", just describe the changes.'
