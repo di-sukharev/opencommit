b7445829c5c6d8c03217445525297353b14cf931
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-09-08 11:24:56 +0200

    The first working e2e test

--- test/e2e/noChanges.test.ts
+++ test/e2e/noChanges.test.ts
@@ -3,14 +3,15 @@ import { prepareEnvironment } from "@gmrchk/cli-testing-library";
 it('run cli flow to get the no changes message', async () => {
   const { spawn, cleanup } = await prepareEnvironment();
 
-  const { waitForFinish, getExitCode } = await spawn(
-    'npm',
-    'start'
+  const { waitForText, waitForFinish, getExitCode } = await spawn(
+    'node',
+    './out/cli.cjs'
   );
 
+  await waitForText("No changes detected")
   await waitForFinish();
 
   expect(getExitCode()).toBe(1);
 
   await cleanup();
-}, 100000);
+});
