9f5e69d5ab3397f92f7817b92d8a7d6ef5983157
Matthew Salter <matthew.salter@dvla.gov.uk>
2023-11-30 10:19:20 +0000

    fix: ET-1441: update the error message to use the correct constant DEFAULT_TOKEN_LIMITS.DEFAULT_MAX_TOKENS_OUTPUT

--- src/generateCommitMessageFromGitDiff.ts
+++ src/generateCommitMessageFromGitDiff.ts
@@ -32,7 +32,7 @@ export enum GenerateCommitMessageErrorEnum {
   tooMuchTokens = 'TOO_MUCH_TOKENS',
   internalError = 'INTERNAL_ERROR',
   emptyMessage = 'EMPTY_MESSAGE',
-  outputTokensTooHigh = `Token limit exceeded, OCO_TOKENS_MAX_OUTPUT must not be much higher than the default ${DEFAULT_MAX_TOKENS_OUTPUT} tokens.`
+  outputTokensTooHigh = `Token limit exceeded, OCO_TOKENS_MAX_OUTPUT must not be much higher than the default ${DEFAULT_TOKEN_LIMITS.DEFAULT_MAX_TOKENS_OUTPUT} tokens.`
 }
 
 const ADJUSTMENT_FACTOR = 20;
