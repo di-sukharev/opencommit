80ca06c56104d1e830af7f59c89acd7fb168070f
Chris Motch <cmotch@gmail.com>
2024-02-20 14:00:14 -0500

    🔥 chore: remove tsup and rimraf dependencies and associated build script
    The `tsup` and `rimraf` dependencies, along with the `build:tsup` script and `tsup.config.ts` file, have been removed to streamline the build process. This change simplifies the build setup by eliminating unnecessary tools and configurations that were not being utilized effectively in the project.

--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig((options) => ({
-  clean: !options.watch,
-  // dts: !options.watch,
-  entry: ['src/cli.ts', 'src/github-action.ts'],
-  format: ['esm']
-}));
