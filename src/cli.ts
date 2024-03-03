9b7337f67f7d433acfa5886bed12d851e17664c1
Malthe Poulsen <30603252+malpou@users.noreply.github.com>
2024-03-03 12:36:55 +0100

    Support Gitmoji Format in Commit Messages (#249)
    * 📝 docs(prompts.ts): update prompt message to include information about GitMoji convention and descriptions of changes
    
    📝 docs(prompts.ts): update prompt message to include information about GitMoji convention and descriptions of changes
    
    * 🎨 (prompts.ts): import `removeConventionalCommitWord` function to remove conventional commit word from commit prompts
    🐛 (prompts.ts): remove conventional commit word from `commitFix` and `commitFeat` prompts to improve clarity
    📝 (removeConventionalCommitWord.ts): add `removeConventionalCommitWord` function to remove conventional commit word from commit message
    
    * 📝 (package.json): update version from 3.0.3 to 3.0.0 to align with the latest release
    
    * 🔧 (cli.ts): add a new flag 'fgm' to the 'flags' object to support the '--fgm' flag in the CLI command
    🔧 (commit.ts): pass the value of the 'fgm' flag to the 'commit' function to enable or disable full GitMoji specification
    ♻️ (commit.ts): refactor the 'commit' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'commit' function recursively
    ♻️ (generateCommitMessageFromGitDiff.ts): refactor the 'generateCommitMessageByDiff' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'generateCommitMessageChatCompletionPrompt' function
    ♻️ (generateCommitMessageFromGitDiff.ts): refactor the 'generateCommitMessageChatCompletionPrompt' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'getMainCommitPrompt' function
    ♻️ (generateCommitMessageFromGitDiff.ts): refactor the 'getCommitMsgsPromisesFromFileDiffs' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'getMessagesPromisesByChangesInFile' function
    ♻️ (generateCommitMessageFromGitDiff.ts): refactor the 'getMessagesPromisesByChangesInFile' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'generateCommitMessageChatCompletionPrompt' function
    ♻️ (prompts.ts): refactor the 'getMainCommitPrompt' function to accept the 'fullGitMojiSpec' parameter and pass it to the 'INIT_MAIN_PROMPT' function
    
    * 📝 (README.md): add documentation for the `--fgm` flag in the `oco` command to enable the use of the full GitMoji specification
    
    * 📝 (README.md): update flag description for using full GitMoji specification
    📝 (README.md): add link to the GitMoji specification for reference
    
    * 🔧 (README.md): fix a typo in the description of the `Use Full GitMoji Specification` flag
    🔧 (api.ts): update the default value of the `apiKey` variable to a placeholder value for testing purposes
    
    * Revert "🔧 (README.md): fix a typo in the description of the `Use Full GitMoji Specification` flag"
    
    This reverts commit 230a4aa449b4718063db22f7aa835bbb68dc1a88.
    
    * 🔧 (README.md): fix a typo in the description of the `Use Full GitMoji Specification` flag
    
    * 📝 (prompts.ts): update INIT_MAIN_PROMPT content to include information about the fullGitMojiSpec flag and provide instructions on how to choose the right emoji for the commit message
    
    ---------
    
    Co-authored-by: GPT10 <57486732+di-sukharev@users.noreply.github.com>

--- src/cli.ts
+++ src/cli.ts
@@ -17,17 +17,19 @@ cli(
     version: packageJSON.version,
     name: 'opencommit',
     commands: [configCommand, hookCommand, commitlintConfigCommand],
-    flags: {},
+    flags: {
+      fgm: Boolean
+    },
     ignoreArgv: (type) => type === 'unknown-flag' || type === 'argument',
     help: { description: packageJSON.description }
   },
-  async () => {
+  async ({ flags }) => {
     await checkIsLatestVersion();
 
     if (await isHookCalled()) {
       prepareCommitMessageHook();
     } else {
-      commit(extraArgs);
+      commit(extraArgs, flags.fgm);
     }
   },
   extraArgs
