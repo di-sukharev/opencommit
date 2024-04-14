673eee209d7e1de843a503d6c9738b18f52f9bff
metavind <94786679+metavind@users.noreply.github.com>
2024-04-14 07:20:12 +0200

    Feat: Add Claude 3 support (#318)
    * 3.0.12
    
    * build
    
    * feat: anthropic claude 3 support
    
    * fix: add system prompt
    
    * fix: type check
    
    * fix: package version
    
    * fix: update anthropic for dependency bug fix
    
    * feat: update build files
    
    * feat: update version number
    
    ---------
    
    Co-authored-by: di-sukharev <dim.sukharev@gmail.com>

--- src/utils/engine.ts
+++ src/utils/engine.ts
@@ -2,12 +2,15 @@ import { AiEngine } from '../engine/Engine';
 import { api } from '../engine/openAi';
 import { getConfig } from '../commands/config';
 import { ollamaAi } from '../engine/ollama';
+import { anthropicAi } from '../engine/anthropic'
 import { testAi } from '../engine/testAi';
 
 export function getEngine(): AiEngine {
   const config = getConfig();
   if (config?.OCO_AI_PROVIDER == 'ollama') {
     return ollamaAi;
+  } else if (config?.OCO_AI_PROVIDER == 'anthropic') {
+    return anthropicAi;
   } else if (config?.OCO_AI_PROVIDER == 'test') {
     return testAi;
   }
