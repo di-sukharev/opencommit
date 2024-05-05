cb7f5dd44d741b3cd9ae3418661a64ba22f198c1
di-sukharev <dim.sukharev@gmail.com>
2024-05-05 19:12:25 +0300

    build

--- out/cli.cjs
+++ out/cli.cjs
@@ -26936,7 +26936,7 @@ function G3(t2, e3) {
 // package.json
 var package_default = {
   name: "opencommit",
-  version: "3.0.14",
+  version: "3.0.15",
   description: "Auto-generate impressive commits in 1 second. Killing lame commits with AI \u{1F92F}\u{1F52B}",
   keywords: [
     "git",
