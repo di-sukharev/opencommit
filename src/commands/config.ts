37fb140563b9e7c425f25426d1c2240f1a30c5a7
Ying-Shan Lin <yslinear@gmail.com>
2024-07-20 16:00:10 +0800

    Update Claude Models (#362)

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -64,9 +64,10 @@ export const MODEL_LIST = {
       "gpt-4o-mini",
       "gpt-4o-mini-2024-07-18"],
 
-  anthropic: ['claude-3-haiku-20240307',
+  anthropic: ['claude-3-5-sonnet-20240620',
+              'claude-3-opus-20240229',
               'claude-3-sonnet-20240229',
-              'claude-3-opus-20240229']
+              'claude-3-haiku-20240307']
 }
 
 const getDefaultModel = (provider: string | undefined): string => {
