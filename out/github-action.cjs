f1338ff53ed15e3fdd660a77311ff8d5289f9214
Florentin Sakwiset <florentin@wemake.cx>
2024-08-31 14:42:27 +0200

    ✨ (commands/config.ts): add new Gemini Pro experimental models to MODEL_LIST

--- out/github-action.cjs
+++ out/github-action.cjs
@@ -51262,7 +51262,10 @@ var MODEL_LIST = {
   ],
   gemini: [
     "gemini-1.5-flash",
+    "gemini-1.5-flash-exp-0827",
     "gemini-1.5-pro",
+    "gemini-1.5-pro-exp-0801",
+    "gemini-1.5-pro-exp-0827",
     "gemini-1.0-pro",
     "gemini-pro-vision",
     "text-embedding-004"
