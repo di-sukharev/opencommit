5fb3d75412762c556b6208819febf5738f039d5d
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2025-04-14 04:57:53 -0400

    fix tests
    Signed-off-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,12 @@
-import 'cli-testing-library/extend-expect'
-import { configure } from 'cli-testing-library'
+// Using dynamic imports for ESM compatibility
+import 'cli-testing-library/extend-expect';
+import { configure } from 'cli-testing-library';
 import { jest } from '@jest/globals';
 
+// Make Jest available globally
 global.jest = jest;
 
 /**
  * Adjusted the wait time for waitFor/findByText to 2000ms, because the default 1000ms makes the test results flaky
  */
-configure({ asyncUtilTimeout: 2000 })
+configure({ asyncUtilTimeout: 2000 });
